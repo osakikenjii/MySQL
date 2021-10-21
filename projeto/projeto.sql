c2d25afa1f0a560c2935a86282fdf9192e66a7b2
Victor kenji osaki figueiredo <73260602+osakikenjii@users.noreply.github.com>
2021-10-20 22:19:22 -0300

    Update projeto.sql

--- projeto/projeto.sql
+++ projeto/projeto.sql
@@ -9,33 +9,28 @@ use dblavarapido;
 create table clientes(
 	 idcli int primary key auto_increment,
 	 nome varchar(50) not null,
-	 telfixo varchar(15) not null,
-	 telcel varchar(15) not null
+	 tel1 varchar(15) not null,
+	 tel2 varchar(15)
  );
-  
-drop table clientes;
-  
+ drop table clientes;
  describe clientes;
-	alter table clientes modify column telfixo int ;
-    alter table clientes modify column telcel int ;
- 
  select * from clientes order by nome;
  
  -- crud
-	 insert into clientes (nome, telfixo, telcel)
-	 values ('Rafael ferreira',1126921514, 1199998875);
+	 insert into clientes (nome, tel1)
+	 values ('Rafael ferreira',1126921514);
 	 
-	  insert into clientes (nome, telfixo, telcel)
+	  insert into clientes (nome, tel1, tel2)
 	 values ('Victor kenji',1125987514, 1199985235);
 	 
-	  insert into clientes (nome, telfixo, telcel)
-	 values ('Jose assis',1189651514, 1165238875);
+	  insert into clientes (nome, tel1)
+	 values ('Jose assis',1189651514);
 	 
-	  insert into clientes (nome, telfixo, telcel)
+	  insert into clientes (nome, tel1, tel2)
 	 values ('Vaamonde',1126927845, 1195214875);
 	 
-	  insert into clientes (nome, telfixo, telcel)
-	 values ('Ramos',1126985234, 1178948875);
+	  insert into clientes (nome, tel1)
+	 values ('Ramos',1126985234);
  
  select*from clientes;
  
@@ -45,82 +40,65 @@ drop table clientes;
 	 modelo varchar(50) not null,
 	 cor varchar(15) not null,
 	 placa varchar(15) not null,
-	 anofab varchar(50) not null,
-	 km varchar(100) not null
+	 km varchar(100) not null,
+     foreign key(idcar) references clientes(idcli)
 );
-
+drop table carros;
 describe carros;
 select*from carros;
     
  -- crud
-	 insert into carros (marca,modelo,placa,cor,km,anofab)
+	 insert into carros (marca,modelo,placa,cor,km)
 	values
-	('Chevrolet','celta','abc123','preto',85.326, 2013);
+	('Chevrolet','celta','abc123','preto',85.326);
 
-	insert into carros (marca,modelo,placa,cor,km,anofab)
+	insert into carros (marca,modelo,placa,cor,km)
 	values
-	('fiat','palio','abc456','vermelho',75.326, 2012);
+	('fiat','palio','abc456','vermelho',75.326);
 
-	insert into carros (marca,modelo,placa,cor,km,anofab)
+	insert into carros (marca,modelo,placa,cor,km)
 	values
-	('ford','ka','abc789','branco',102.566, 2009);
+	('ford','ka','abc789','branco',102.566);
 
-	insert into carros (marca,modelo,placa,cor,km,anofab)
+	insert into carros (marca,modelo,placa,cor,km)
 	values
-	('toyota','corolla','dfg123','prata',21.210, 2021);
+	('toyota','corolla','dfg123','prata',21.210);
 
-	insert into carros (marca,modelo,placa,cor,km,anofab)
+	insert into carros (marca,modelo,placa,cor,km)
 	values
-	('vowksvagem','gol','cvb468','verde',110.326, 2000);
+	('vowksvagem','gol','cvb468','verde',110.326);
 
-create table servicos(
-	 service int primary key auto_increment,
-	 lavsimp varchar(50) not null,
-	 lavcomp varchar(50) not null,
-	 lavmotor varchar(50) not null,
-	 polimento varchar(50) not null,
-	 higienizacao varchar(50) not null
- );
  
 create table tbos (
 	os int primary key auto_increment,
     dataos timestamp default current_timestamp,
-    tipo varchar(20) not null,
-    statusos varchar(30) not null,
     serviço varchar(200) not null,
     valor decimal(10,2),
     idcli int not null,
     foreign key(idcli) references clientes(idcli)	
 );
-
+drop table tbos;
 -- data de saida date not null 
 
-insert into tbos(tipo,statusos,serviço,valor,idcli)
+insert into tbos(serviço,valor,idcli)
 values
-('serviço','lavando','lavagem simples',10.00,1);
+('lavagem simples',10.00,1);
 
-insert into tbos(tipo,statusos,serviço,valor,idcli)
+insert into tbos(serviço,valor,idcli)
 values
-('serviço','polindo','polimento',150.00,2);
+('polimento',150.00,2);
 
-insert into tbos(tipo,statusos,serviço,valor,idcli)
+insert into tbos(serviço,valor,idcli)
 values
-('orçamento','valando','lavagem completa',30.00,2);
+('lavagem completa',30.00,3);
 
-insert into tbos(tipo,statusos,serviço,valor,idcli)
-values('orçamento','aguardando aprovação','higienizacão',110.00,4);
-
-insert into tbos(tipo,statusos,serviço,valor,idcli)
+insert into tbos(serviço,valor,idcli)
 values
-('serviço','retirado','higienizacão',110.00,5);
+('higienizacão',120.00,4);
 
-insert into tbos(tipo,statusos,serviço,valor,idcli)
+insert into tbos(serviço,valor,idcli)
 values
-('serviço','retirado','polimento',150.00,5);
-
-insert into tbos(tipo,statusos,serviço,valor,idcli)
-values
-('serviço','retirado','levagem simples',10.00,5);
+('lavagem de motor',110.00,5);
 
 delete from tbos where os=1;
 delete from tbos where os=2;
@@ -128,21 +106,11 @@ delete from tbos where os=2;
 describe tbos;
 select * from tbos;
 
-update tbos set statusos='lavando' where os = 8;
-update tbos set statusos='lavando' where os = 3;
-update tbos set statusos='aguardando aprovação' where os = 3;
-update tbos set valor=150.00 where os = 6;
-update tbos set valor=10.00 where os = 7;
-update tbos set serviço='lavagem de motor' where os = 7;
-update tbos set idcli=1 where os = 6;
-update tbos set idcli=2 where os = 7;
-
 select
-tbos.dataos,tbos.tipo,tbos.statusos as status_os,tbos.serviço, tbos.valor,
+tbos.dataos as tbos.serviço, tbos.valor,
 clientes.nome,
-clientes.telfixo, 
-clientes.telcel,
-clientes.idcli,
+clientes.tel1, 
+clientes.tel2,
 carros.marca,
 carros.modelo
 from tbos inner join clientes inner join carros
